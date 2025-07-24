c52b3efb8ec159f276e811562a6600afafe86696
TaniyaTaragi <taniyataragi10@gmail.com>
2025-07-24 19:45:23 +0530

    update a problem

--- Arrays/MediumArrayProblems/LeadrersInAnArray.cpp
+++ Arrays/MediumArrayProblems/LeadrersInAnArray.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
+
 using namespace std;
 int main()
 {
@@ -9,22 +11,39 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
          cin>>arr[i];
-    for(int i=0,j=n-1;i<n;)
-    {
-        if(arr[i]>arr[j]&&j!=(i+1))
-            j--;
-        else if((i+1)==j&&arr[i]>arr[j])
-        {
-            cout<<arr[i]<<" ";
-            i++;
-             j=n-1;
+    // for(int i=0,j=n-1;i<n;)
+    // {
+    //     if(arr[i]>arr[j]&&j!=(i+1))
+    //         j--;
+    //     else if((i+1)==j&&arr[i]>arr[j])
+    //     {
+    //         cout<<arr[i]<<" ";
+    //         i++;
+    //          j=n-1;
 
-        }
-        else
+    //     }
+    //     else
+    //       {
+    //         i++;
+    //          j=n-1;
+    //       }
+    // }
+    // cout<<arr[n-1];
+    int maxval=arr[n-1];
+    vector<int>res;
+    res.push_back(maxval);
+
+    for(int i=n-2;i>=0;i--)
+    {
+        if(arr[i]>maxval)
           {
-            i++;
-             j=n-1;
+              res.push_back(arr[i]);
+              maxval=arr[i];
           }
     }
-    cout<<arr[n-1];
+    for(int i=0;i<res.size();i++)
+    {
+        cout<<res[i]<<" ";
+    }
+    
 }
