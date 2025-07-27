54ab180374dc1b7c97cac6b6bd5b2d95c6fd0153
TaniyaTaragi <taniyataragi10@gmail.com>
2025-07-27 22:26:05 +0530

    uploaded medium array problem

---
+++ Arrays/MediumArrayProblems/CountSubarraySumEqualToK.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+int main()
+{
+      int n;
+      cout<<"Enter the size of array :";
+      cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+         cin>>arr[i];
+   
+
+}
