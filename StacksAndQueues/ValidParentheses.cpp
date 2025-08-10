219952974bb8121e4b3dd2e05483f4814adad22f
Taniya Taragi <taniyataragi10@gmail.com>
2025-08-10 13:15:32 +0530

    Create ValidParentheses.cpp

---
+++ StacksAndQueues/ValidParentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2!=0)return false;
+        if(s[0]=='}'||s[0]==']'||s[0]==')')
+             return false;
+        stack<char>st;
+        for(int i=0;i<s.length();i++)
+        {
+
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+               st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+            if(st.top()=='('&&s[i]==')')
+              st.pop();
+            else if(st.top()=='{'&&s[i]=='}')
+              st.pop();
+            else if(st.top()=='['&&s[i]==']')
+              st.pop();
+            else
+             return false;
+        }
+           
+        }
+        if(st.empty())
+           return true;
+        else
+         return false;
+    }
+};
